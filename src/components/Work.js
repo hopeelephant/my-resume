8a9fb172b4d71a52527b0d5d5f1bbba2318e4600
hopeelephant <765846396@qq.com>
2017-03-24 00:45:53 +0800

    second

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,24 +9,20 @@ class Work extends React.Component {
         }
     }
     componentDidMount() {
-        axios.get("https://raw.githubusercontent.com/yewenxiang23/my-resume/master/src/blogData/product.json").then(res => this.setState({productDate: res.data}));
+        axios.get("https://github.com/hopeelephant/my-resume/blob/master/blog/work.json").then(res => this.setState({productDate: res.productDate}));
     }
     render() {
         return (
             <div className="production">
                 <section className="work">
                     {this.state.productDate.length == 0
-                        ? <div className="spinPosition">
-                                <Spin size="large" tip="Loading..."/>
-                            </div>
-                        : this.state.productDate.map((item) => (
+                        ? "请稍后" : this.state.productDate.map((item) => (
                             <figure className="product" key={Math.random()}>
                                 <h4>{item.title}</h4>
-                                <img src={`http://omfmbv825.bkt.clouddn.com/${item.imgName}.png`} alt={item.imgName}/>
-                                <div className="product-title">
-                                    <a href={`https://yewenxiang23.github.io/${item.demoAddress}`}>演示demo</a>
-                                    <a href={`https://github.com/yewenxiang23/${item.codeAddress}`}>源码地址</a>
-                                </div>
+                                <a href={`https://hopeelephant.github.io/${item.demoAddress}`}>
+                                  <img src={`https://hopeelephant.github.io/my-resume/src/components/images/${item.imgName}.png`} alt={item.imgName}/>
+                                </a>
+
                                 <figcaption>{item.desc}</figcaption>
                             </figure>
                         ))
